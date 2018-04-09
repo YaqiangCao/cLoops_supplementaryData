1e105c6d904890c1b5a3cabd2b03d97fbfc1a1e7
YaqiangCao <caoyaqiang0410@gmail.com>
2018-04-09 20:31:35 +0800

    v0.9

---
+++ SupplementaryData/loops/ChIA-PET/run.sh
@@ -0,0 +1,8 @@
+cLoops -f ../1.bedpes/2.cis/GM12878_CTCF.bedpe -o GM12878_CTCF -p 6 -m 1 -j 
+cLoops -f ../1.bedpes/2.cis/GM12878_RAD21.bedpe -o GM12878_RAD21 -j -p 6 -m 1
+cLoops -f ../1.bedpes/2.cis/HeLa_CTCF.bedpe -o HeLa_CTCF -j -p 6 -m 1
+cLoops -f ../1.bedpes/2.cis/K562_RAD21.bedpe -o K562_RAD21 -j -p 6 -m 1
+cLoops -f ../1.bedpes/2.cis/K562_POL2RA.bedpe -o K562_POL2RA -j -p 6 -m 1
+cLoops -f ../1.bedpes/2.cis/K562_H3K27ac.bedpe -o K562_H3K27ac -j -p 6 -m 2
+cLoops -f ../1.bedpes/2.cis/K562_H3K4me1.bedpe -o K562_H3K4me1 -j -p 6 -m 2
+cLoops -f ../1.bedpes/2.cis/K562_H3K4me3.bedpe -o K562_H3K4me3 -j -p 6 -m 1
