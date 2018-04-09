f0939d2b88f3c1b3ed613e4dcd597514fce1af15
YaqiangCao <caoyaqiang0410@gmail.com>
2018-04-09 20:32:58 +0800

    v0.9

---
+++ SupplementaryData/loops/Hi-C/run.sh
@@ -0,0 +1,2 @@
+cLoops -f ../2.bedpesHiCPro/GM12878_HiC_GSM1551552.bedpe.gz -o GM12878_HiC -p 2 -j -eps 5000,7500,10000 -minPts 20,30,40,50 -hic 
+cLoops -f ../2.bedpesHiCPro/K562_HiC_GSM1551619.bedpe.gz -o K562_HiC -p 2 -j -eps 5000,7500,10000 -minPts 20,30,40,50 -hic 
